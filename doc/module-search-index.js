6a4e04e3f3adcd136cf304e35477aa3590400549
jlbeo <jlbeo@PC_ASUS>
2026-01-08 20:30:18 +0100

    Subida de carpeta doc y archivos main y ccuentas

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"Cuentas"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Cuentas","l":"cuentas"}];updateSearchResults();
